577ad395ecc6250ee2acb23e55f25eb2205192b2
CaptainHook <moradisarah2001@gmail.com>
2023-05-15 22:48:43 +0330

    complete social media links

---
+++ src/components/social-media/SocialMedias.jsx
@@ -0,0 +1,45 @@
+import classes from './social-medias.module.css'
+import Gmail from '../../assets/images/social-medias/gmail.svg'
+import Github from '../../assets/images/social-medias/github.svg'
+import Instagram from '../../assets/images/social-medias/instagram.svg'
+import Linkedin from '../../assets/images/social-medias/linkedin.svg'
+
+const SocialMedias = () => {
+  const socialDetails = [
+    {
+      icon: Gmail,
+      link: 'mailto:moradisarah2001@gmail.com',
+    },
+    {
+      icon: Github,
+      link: 'https://github.com/SarahMoradi',
+    },
+    {
+      icon: Instagram,
+      link: 'https://instagram.com/sarah_moradii?igshid=Yjk4NWM2ZWVkMw==',
+    },
+    {
+      icon: Linkedin,
+      link: 'https://www.linkedin.com/in/sarah-moradii/',
+    },
+  ]
+  return (
+    <div className={classes.social_medias_container}>
+      <div>
+        <h1>Social Medias</h1>
+      </div>
+      <div className={classes.social_icons_container}>
+        {socialDetails.map((data) => {
+          return (
+            <div className={classes.logo_container}>
+              <a href={data.link}>
+                <img src={data.icon} width={50} />
+              </a>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+export default SocialMedias
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react'
 import Teaser from '../../components/teaser/Teaser'
 import Experiences from '../../components/experiences/Experiences'
 import Skills from '../../components/skills/Skills'
+import SocialMedias from '../../components/social-media/SocialMedias'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Teaser />
       <Skills />
       <Experiences />
+      <SocialMedias />
     </Fragment>
   )
 }
