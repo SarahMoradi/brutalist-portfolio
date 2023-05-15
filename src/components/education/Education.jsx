2533fb8df4c21cac55bb2558d5ff87f93ad9baa7
CaptainHook <moradisarah2001@gmail.com>
2023-05-15 23:20:39 +0330

    complete education section

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,10 +1,39 @@
 import classes from './education.module.css'
+import logo from '../../assets/images/shariaty.png'
+import institute from '../../assets/images/avaye-elm.jpg'
 
 const Education = () => {
   return (
     <div className={classes.education_container}>
       <div>
         <h1>Education</h1>
+      </div>
+      <div className={classes.course_container}>
+        <div>
+          <img src={logo} width={50} />
+        </div>
+        <div>
+          <h4>Shariaty Technical and vocational University</h4>
+          <p>Associated degree</p>
+        </div>
+      </div>
+      <div className={classes.course_container}>
+        <div>
+          <img src={logo} width={50} />
+        </div>
+        <div>
+          <h4>Shariaty Technical and vocational University</h4>
+          <p>Bachelor's degree</p>
+        </div>
+      </div>
+      <div className={classes.course_container}>
+        <div>
+          <img src={institute} width={50} />
+        </div>
+        <div>
+          <h4>Avaye Elm Language Institute</h4>
+          <p>completed IELTS course by 2020</p>
+        </div>
       </div>
     </div>
   )
